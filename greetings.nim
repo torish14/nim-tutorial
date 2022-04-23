205cce9e23477300342094845e8352b39dd85ec6
torish14 <nanamin.104@icloud.com>
2022-04-23 22:38:01 +0900

    👗 style: formated nim code to run `nimpretty --indent:2 greetings.nim`

--- greetings.nim
+++ greetings.nim
@@ -3,8 +3,8 @@ echo "What is your name?"
 # var は再代入できる変数
 var name: string = readLine(stdin)
 if name == "":
- echo "You lost your name?"
+  echo "You lost your name?"
 elif name == "name":
- echo "No kidding! Are you name is name?"
+  echo "No kidding! Are you name is name?"
 else:
- echo "Hello, ", name, "!"
+  echo "Hello, ", name, "!"
